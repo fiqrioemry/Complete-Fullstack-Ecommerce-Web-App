57db45978e8e7800bc3009a7a70fa63b5d548c24
fiqrioemry <foemry@gmail.com>
2024-11-27 23:51:37 +0700

    add checkout page

--- app/(nondasboard)/cart/page.jsx
+++ app/(nondasboard)/cart/page.jsx
@@ -1,18 +1,40 @@
-import SectionHead from "@/components/common/SectionHead";
-import React from "react";
+"use client";
 
-const page = () => {
+import React from "react";
+import { useSelector } from "react-redux";
+import SectionHead from "@/components/common/SectionHead";
+
+const Page = () => {
+  const { cart } = useSelector((state) => state.cart);
+  console.log(cart);
   return (
     <section className="py-12">
-      <div className="container mx-auto">
+      <div className="container mx-auto space-y-4">
         <SectionHead title="Your Cart" />
-        <div className="flex flex-wrap">
-          {/* product detail */}
-          <div></div>
+        <div className="flex flex-wrap ">
+          {/* shopping cart*/}
+          <div className="order-2 w-full md:order-1 md:w-[70%] pr-6">
+            <div className="space-y-2">
+              <div className="py-2 px-4 borders flex items-center space-x-4">
+                <input type="checkbox" />
+                <div>Select All</div>
+              </div>
+
+              <div className="py-2 px-4 borders flex items-center space-x-4">
+                <input type="checkbox" />
+                <div>Select All</div>
+              </div>
+            </div>
+          </div>
+
+          {/* shopping summary*/}
+          <div className="order-2 w-full md:order-1 md:w-[30%]">
+            <div className="borders h-[200px]"></div>
+          </div>
         </div>
       </div>
     </section>
   );
 };
 
-export default page;
+export default Page;
